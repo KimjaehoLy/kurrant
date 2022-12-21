40b15e29a155f7546b75b1a3c561f469247b01bf
KimjaehoLy <kjaeho8389@naver.com>
2022-12-21 22:39:13 +0900

    과제 초기 파일 커밋

---
+++ src/main/java/com/assignment/kurrant/KurrantApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.kurrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurrantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KurrantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assignment/kurrant/KurrantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.kurrant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurrantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
